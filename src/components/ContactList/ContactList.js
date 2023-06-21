d63ab2a74d9ea9bf498675664cac685531576983
AnnaBoldyreva1505 <hannabl1988@gmail.com>
2023-06-21 22:22:44 +0600

    upd

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,25 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from '../../redux/operations';
-import { selectContacts } from '../../redux/contacts/contact-selectors';
-import { selectFilter } from '../../redux/filter/filter-selectors';
+import { selectVisibleContacts } from '../../redux/contacts/contact-selectors';
 import toast from 'react-hot-toast';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
+  const contactsList = useSelector(selectVisibleContacts);
 
-  const getVisibleContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-
-    return contacts.filter(({ name }) =>
-      name.toLowerCase().includes(normalizedFilter)
-    );
-  };
 
   return (
     <ul>
-      {getVisibleContacts().map(({ name, id, phone }) => {
+      {contactsList.map(({ name, id, phone }) => {
         return (
           <li key={id}>
             {name}: {phone}
